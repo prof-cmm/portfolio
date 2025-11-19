2aa5fea0811e52924aad6aa0cbbe23ecf1e16008
prof-cmm <carolmontrezol@gmail.com>
2025-11-19 14:36:40 -0300

    aula 2 19.11

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ let texto = document.querySelector("#nome");
 
 let nome = prompt("Qual é o seu nome?");
 texto.textContent = nome;
+
+if (nome == null){
+    texto.textContent = 'Seja bem-vindo';
+}else{
+    texto.textContent = nome;
+}
